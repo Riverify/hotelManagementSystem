4a6e9c39df4c6c67905df5d84c2ee705f1981684
Riverify <CryingRiver@foxmail.com>
2022-06-06 21:07:04 +0800

    first commit

--- DBeaver/Scripts/Script.sql
+++ DBeaver/Scripts/Script.sql
@@ -1,5 +1,6 @@
 -- RiverLuo
 
+-- 房间表
 CREATE table roomInfo
 (
     roomno   int(3) primary key,
@@ -9,20 +10,21 @@ CREATE table roomInfo
     booked   boolean default false
 );
 
-
+-- 用户表
 create table customer
 (
-    id    int(4) primary key auto_increment,
-    name  varchar(4) not null,
-    idnum varchar(18),
-    phone int(11),
-    vip   boolean default true,
-    money int(9),
+    id       int(4) primary key auto_increment,
+    name     varchar(4) not null,
+    idnum    varchar(18),
+    phone    varchar(11),
+    vip      boolean default false,
+    money    int(9) default 0,
+    password varchar(20),
     constraint uk_customer_idnum unique (idnum),
     constraint uk_customer_phone unique (phone)
 );
 
-
+-- 账单表
 create table roomOperation
 (
     business  int(5) primary key auto_increment,
@@ -30,11 +32,43 @@ create table roomOperation
     roomno    int(3),
     enterdate date,
     outdate   date,
-    status    varchar(2) check (status = '预定' or status = '入住' or status = '无人'),
+    -- status varchar(2) check(status = '预定' or status = '入住' or status = '无人'),
     constraint fk_customer_roomInfo foreign key (roomno) references roomInfo (roomno),
     constraint fk_customer_roomOp foreign key (idnum) references customer (idnum)
 
 );
+
+-- 细节概览视图
+create
+or replace view detailAll
+as
+select ro.business,
+       c.name,
+       c.phone,
+       c.idnum,
+       ro.roomno,
+       ro.outdate - ro.enterdate as orderday,
+       case
+           when enterdate > CURRENT_DATE() then "预定"
+           when enterdate <= CURRENT_DATE() and outdate >= CURRENT_DATE() then "在住"
+           when outdate < CURRENT_DATE() then "无人"
+           else "无人"
+           end                   as status
+        ,
+       ri.roomtype,
+       ri.bednum,
+       ri.price
+from customer c
+         join roomOperation ro
+              on (c.idnum = ro.idnum)
+         join roomInfo ri
+              on (ri.roomno = ro.roomno)
+where c.vip = true
+order by ro.business;
+
+-- 测试数据
+-- insert into roomOperation values(null, '342223200201266631', 101, '2022-06-04', '2022-06-05');
+
 
 insert into roomInfo
 values (101, '普通房', 2, 120, false);
@@ -59,8 +93,17 @@ values (110, '总统房', 2, 1120, false);
 insert into roomInfo
 values (111, '廉价房', 2, 50, false);
 
+-- 管理员
 insert into customer
-values (1, 'admin', 111111111111111111,);
+values (1, 'ad', 'admin', 'admin', true, 1000000);
+
+
+-- ---------------------------------------------------------------------------
+
+select *
+from customer
+where phone = "19355359399"
+  and password = "114514"
 
 
 
